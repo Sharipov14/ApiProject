678a6196ef24837416d32c95cd9c686327f4b690
mahmadullo56@gmail.com <mahmadullo56@gmail.com>
2021-08-16 13:58:21 +0400

    Удаление ненужных файлов

--- ClientApp/src/app/projects/project.js
+++
@@ -1,7 +0,0 @@
-export class Project {
-    constructor(projectId, name) {
-        this.projectId = projectId;
-        this.name = name;
-    }
-}
-//# sourceMappingURL=project.js.map
--- ClientApp/src/app/workers/worker.js
+++
@@ -1,8 +0,0 @@
-export class Worker {
-    constructor(workerId, fullName, position) {
-        this.workerId = workerId;
-        this.fullName = fullName;
-        this.position = position;
-    }
-}
-//# sourceMappingURL=worker.js.map
